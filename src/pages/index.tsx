7a6799c8c3a422f435fca106a951acfc2b1fee34
longhaiyan <1250766229@qq.com>
2019-06-12 23:58:30 +0800

    add: index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,19 +6,40 @@ class Index extends React.Component {
     const canvas = document.getElementById("J_canvas");
     if (canvas.getContext) {
       const ctx = canvas.getContext("2d");
+      const goldPoiont = 889
+      const width = document
 
-      ctx.fillStyle = "rgb(200,0,0)";
-      ctx.fillRect (10, 10, 55, 50);
+      ctx.fillStyle = "#FFA500"
+      ctx.beginPath();
+      ctx.moveTo(0, 0);
+      ctx.lineTo(goldPoiont, 0);
+      ctx.lineTo(1440 - goldPoiont, 789);
+      ctx.lineTo(0, 789);
+      ctx.fill();
 
-      ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
-      ctx.fillRect (30, 30, 55, 50);
+      ctx.fillStyle = "#dd4a68"
+      ctx.beginPath();
+      ctx.moveTo(goldPoiont, 0);
+      ctx.lineTo(1440, 0);
+      ctx.lineTo(1440, 789);
+      ctx.lineTo(1440 - goldPoiont, 789);
+      ctx.fill();
+
+      // ctx.beginPath();
+      // ctx.moveTo(0, 0);
+      // ctx.lineTo(100, 0);
+      // ctx.lineTo(100, 25);
+      // ctx.fill();
     }
   }
-  render() {
+  componentDidMount() {
+    console.log("画")
     this.draw()
+  }
+  render() {
     return (
     <div>
-      <canvas id="J_canvas" width="500px" height="500px"></canvas>
+      <canvas id="J_canvas" width="1440px" height="789px"></canvas>
     </div>
     );
   }
